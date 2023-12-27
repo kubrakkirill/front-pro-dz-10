8b124329e9b6d7367e5d77c516c1a77e498787c7
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-12-27 16:12:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const number = parseFloat(prompt("Enter a number:"));
+
+if (isNaN(number)) {
+    alert("Please enter a valid number");
+} else {
+    const degree = parseInt(prompt("Enter a degree (integer):"));
+
+    if (isNaN(degree) || !Number.isInteger(degree)) {
+        alert("Please enter a valid integer degree");
+    } else {
+        function pow(number, degree) {
+            if (degree === 0) {
+                return 1;
+            } else if (degree === 1) {
+                return number;
+            } else if (degree < 0) {
+                return 1 / pow(number, -degree);
+            } else {
+                return number * pow(number, degree - 1);
+            }
+        }
+
+        alert(`${number}^${degree} = ${pow(number, degree)}`);
+    }
+}
+
